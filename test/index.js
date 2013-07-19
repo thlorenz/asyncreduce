b7a6d8be3c7a95bb4be3f3bd7cd55c626e23d502
Thorsten Lorenz <thlorenz@gmx.de>
2013-07-19 09:28:39 -0400

    adapting tests to work in browser and including 'run tape' script

--- test/index.js
+++ test/index.js
@@ -5,22 +5,55 @@ var test = require('tape')
 
 var fs = require('fs');
 var path = require('path');
-var asyncReduce = require('..');
+var asyncReduce = require('../');
 
-test('\nreducing files to a hash with their modes', function (t) {
+if (typeof window === 'undefined') {
+  test('\nreducing files to a hash with their modes', function (t) {
+    t.plan(2)
+    asyncReduce(
+        [ 'test', 'examples', '.travis.yml', 'index.js', 'Readme.md' ]
+      , {}
+      , function size (acc, file, cb) {
+          var p = path.join(__dirname, '..', file);
+
+          fs.stat(p, function (err, stat) {
+            if (err) return cb(err);
+
+            acc[file] = stat.mode;
+            cb(null, acc);
+          });
+        }
+      , function done (err, acc) {
+          t.notOk(err, 'no error')
+          t.deepEqual(
+              acc
+            , { test: 16877,
+                examples: 16877,
+                '.travis.yml': 33188,
+                'index.js': 33188,
+                'Readme.md': 33188 }
+          )
+        }
+    )
+  })
+}
+
+test('\nsimulate reducing files to a hash with their modes', function (t) {
   t.plan(2)
+  var sizes = {
+    test          :  16877,
+    examples      :  16877,
+    '.travis.yml' :  33188,
+    'index.js'    :  33188,
+    'Readme.md'   :  33188 }
+
   asyncReduce(
       [ 'test', 'examples', '.travis.yml', 'index.js', 'Readme.md' ]
     , {}
     , function size (acc, file, cb) {
-        var p = path.join(__dirname, '..', file);
-
-        fs.stat(p, function (err, stat) {
-          if (err) return cb(err);
-
-          acc[file] = stat.mode;
-          cb(null, acc);
-        });
+        var fileSize = sizes[file]
+        acc[file] = fileSize;
+        setTimeout(cb.bind(null, null, acc), 5);
       }
     , function done (err, acc) {
         t.notOk(err, 'no error')
